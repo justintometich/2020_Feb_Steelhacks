363fc5f707b12610a101eb1d94c3e8ea9adf3c5e
Wenyu Li <wel104@pitt.edu>
2020-02-14 22:33:51 -0500

    map

---
+++ app/src/main/java/com/example/a2020_feb_steelhacks/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a2020_feb_steelhacks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
