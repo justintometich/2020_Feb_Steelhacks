f128602557dae2a87f21e440192e70ff9742f33f
justintometich <39749184+justintometich@users.noreply.github.com>
2020-02-14 22:47:21 -0500

    cleaning

--- app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
+++ app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
@@ -2,12 +2,9 @@ package com.example.a2020_feb_steelhacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import android.text.Editable
 import kotlinx.android.synthetic.main.activity_main.*
-=======
 import okhttp3.*
->>>>>>> b1ac7322b9d4361a615863a40d1d68dc83bbe5f1
 
 class MainActivity : AppCompatActivity() {
 
