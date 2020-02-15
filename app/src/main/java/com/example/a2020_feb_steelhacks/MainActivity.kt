80e5d2e7513299198b32526a8018f69d6eb6111d
justintometich <39749184+justintometich@users.noreply.github.com>
2020-02-15 13:32:08 -0500

    added relative coordinate snap locations

--- app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
+++ app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
@@ -12,11 +12,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.json.JSONObject
+import org.jsoup.Jsoup
 import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
-    var userSearchQueries = arrayOfNulls<String>(3)
+    //var userSearchQueries = arrayOfNulls<String>(3)
 
     var client = OkHttpClient()
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
             findProduct(productName, storeId)
         }
 
+        comparePriceToAmazon("apples", 7.5.toFloat())
         //findProduct("toilet+paper", "2757")
 
         //locationIcon.x = mensBtn.marginLeft.toFloat()
@@ -66,21 +68,33 @@ class MainActivity : AppCompatActivity() {
                         try {
                             productTcin = item.getJSONObject(0).getString("tcin")
                         } catch (e: Error) {
-
+                            println(e)
                         }
                         targetGetRequest(productTcin) {
-                            if (it%4 > 2) {
-                                locationIcon.x = mensBtn.marginLeft.toFloat()
-                                locationIcon.y = mensBtn.marginTop.toFloat()
-                            } else if (it%4 > 1) {
-                                locationIcon.x = menswearBtn.marginLeft.toFloat() + 50.toFloat()
-                                locationIcon.y = menswearBtn.marginTop.toFloat() - 100.toFloat()
-                            } else if (it%4 > 0) {
-                                locationIcon.x = shoesBtn.marginLeft.toFloat()
-                                locationIcon.y = shoesBtn.marginTop.toFloat() - 90.toFloat()
+                            if (it%8 > 6) {
+                                locationIcon.x = intimateBtn.marginLeft.toFloat() + intimateBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = intimateBtn.marginTop.toFloat() - intimateBtn.height.toFloat()/2.toFloat()
+                            } else if (it%8 > 5){
+                                locationIcon.x = sportingGoodsBtn.marginLeft.toFloat() + sportingGoodsBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = sportingGoodsBtn.marginTop.toFloat() - sportingGoodsBtn.height.toFloat()/2.toFloat()
+                            } else if (it%8 > 4){
+                                locationIcon.x = infantsBtn.marginLeft.toFloat() + infantsBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = infantsBtn.marginTop.toFloat() - infantsBtn.height.toFloat()/2.toFloat()
+                            } else if (it%8 > 3){
+                                locationIcon.x = cosmeticsBtn.marginLeft.toFloat() + cosmeticsBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = cosmeticsBtn.marginTop.toFloat() - cosmeticsBtn.height.toFloat()/2.toFloat()
+                            } else if (it%8 > 2) {
+                                locationIcon.x = mensBtn.marginLeft.toFloat() + mensBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = mensBtn.marginTop.toFloat() - mensBtn.height.toFloat()/2.toFloat()
+                            } else if (it%8 > 1) {
+                                locationIcon.x = menswearBtn.marginLeft.toFloat() + menswearBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = menswearBtn.marginTop.toFloat() - menswearBtn.height.toFloat()/2.toFloat()
+                            } else if (it%8 > 0) {
+                                locationIcon.x = shoesBtn.marginLeft.toFloat() + shoesBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = shoesBtn.marginTop.toFloat() - shoesBtn.height.toFloat()/2.toFloat()
                             } else {
-                                locationIcon.x = homeStorageBtn.marginLeft.toFloat()
-                                locationIcon.y = homeStorageBtn.marginTop.toFloat() - 150.toFloat()
+                                locationIcon.x = homeStorageBtn.marginLeft.toFloat() + homeStorageBtn.width.toFloat()/2.toFloat()
+                                locationIcon.y = homeStorageBtn.marginTop.toFloat() - homeStorageBtn.height.toFloat()/2.toFloat()
                             }
                         }
                     }
@@ -130,6 +144,13 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    fun comparePriceToAmazon(productName: String, targetPrice: Float){
+        var doc = Jsoup.connect("https://www.amazon.com/s?k=$productName&ref=nb_sb_noss_2").get()
+        var element = doc.select("[data-cel-widget=\"search_result_3\"]")
+        println(element)
+
     }
 
     fun snapMarkerToButton(location: String?){
