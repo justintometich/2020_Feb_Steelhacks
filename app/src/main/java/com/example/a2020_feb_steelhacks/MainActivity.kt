6cdd481b2e008d2801b0385602bdce4f70833a5b
Khalid Mirza <khm18@pitt.edu>
2020-02-14 23:13:54 -0500

    Added exit button functionality

--- app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
+++ app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.app.AlertDialog
+import android.content.DialogInterface
 import okhttp3.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         button1.setOnClickListener {
             editText2.setText("bla")
+        }
+
+        //Get reference to button
+        val btn = findViewById(R.id.quitButton) as Button
+        //set on-click listener
+        btn.setOnClickListener {
+            //code to run
+
+            //Build alert dialogue -> to be CREATED LATER
+            val d = AlertDialog.Builder(this)
+            //set message of box
+            d.setMessage("Wanna close this?")
+                //If cancellable
+                .setCancelable(false)
+                //positive button
+                .setPositiveButton("Proceed", DialogInterface.OnClickListener { dialog, id ->
+                    finish()
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
+                    dialog.cancel()
+                })
+
+            val alert = d.create()
+            //Set title
+            alert.setTitle("Exit?")
+            //finally, SHOW IT
+            alert.show()
         }
     }
 
