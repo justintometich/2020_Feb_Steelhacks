ddbb4fc551b85131b1b5f24b8a72bda9fd0993a6
Wenyu Li <wel104@pitt.edu>
2020-02-14 22:30:35 -0500

    add map

--- app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
+++ app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.a2020_feb_steelhacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button1.setOnClickListener {
+            editText2.setText("bla")
+        }
     }
 }
