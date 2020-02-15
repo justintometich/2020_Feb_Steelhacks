b1ac7322b9d4361a615863a40d1d68dc83bbe5f1
justintometich <39749184+justintometich@users.noreply.github.com>
2020-02-14 22:32:15 -0500

    testing branch

--- app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
+++ app/src/main/java/com/example/a2020_feb_steelhacks/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.a2020_feb_steelhacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun searchButtonHandler(){
+
+    }
+
+    fun targetGetRequest(userQuery: String?){
+
     }
 }
